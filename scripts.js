48cca7e6f7b74124343cf7123f6e04b9e15f4477
Roopika Risam <rrisam@salemstate.edu>
2019-06-20 20:39:33 -0400

    wrote burrito scolding program

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("scripts.js has loaded");
+let userInput;
+userInput = prompt("what do you want for dinner");
+if(userInput === "burrito"){
+  $("#response").html("Brilliant Choice");
+}  else {
+    $("#response").html("Don't you want a burrito");
+}
