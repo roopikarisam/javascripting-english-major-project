6c9eb0c17a66d16232b093e6bf45bc60b0514e34
Roopika Risam <rrisam@salemstate.edu>
2019-06-21 18:58:24 -0400

    wrote 1 through prompt number program

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-let userInput;
-userInput = prompt("what do you want for dinner");
-if(userInput === "burrito"){
-  $("#response").html("Brilliant Choice");
-}  else {
-    $("#response").html("Don't you want a burrito");
+let i = prompt("givemeanumber");
+let x = 0;
+
+for (i ; x < i;){
+  $("#response").append("<br />" + (x + 1));
+  x = x + 1;
 }
