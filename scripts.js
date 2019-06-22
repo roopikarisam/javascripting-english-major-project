87acc24558d0b41993d5d516b161bb4ba99aaf02
Roopika Risam <rrisam@salemstate.edu>
2019-06-22 06:39:28 -0400

    wrote program counting up & printing odd/even for user input

--- scripts.js
+++ scripts.js
@@ -1,7 +1,66 @@
 let i = prompt("givemeanumber");
 let x = 0;
+//let y = i % 2;
 
-for (i ; x < i;){
-  $("#response").append("<br />" + (x + 1));
-  x = x + 1;
+for (x  ; x <= i; x++) { // initializes var x, sets testing condition of when x is greater than or equal to i so the loop runs when x is less than i and stops when x = i, indicates that each time var x is run through the loop it increases by 1
+  if (x % 2 === 0) { // the loop iterates over x so we want to test x for its modulus - remainder (in this case when divided by 2) - if the modulus === 0 it's an even number) [my error before was using i]
+  $("#response").append("<br />" + (x) + " is even"); //prints even if condition (x % 2 === 0) is met
+  }
+  else {  // if first condition isn't met, prints odd
+  $("#response").append("<br />" + (x) + " is odd");
 }
+} // this code also works and removes unnecessary line
+
+
+// THIS CODE ACTUALLY WORKS
+//for (x  ; x <= i; x++) {
+//  if (x === 0) {
+//    $("#response").append("<br />" + (x) + " is even");
+//  }
+//  else if (x % 2 === 0) {
+//  $("#response").append("<br />" + (x) + " is even");
+//  }
+//  else {
+//  $("#response").append("<br />" + (x) + " is odd");
+//}
+//}
+
+//for (i ; (x + 1) <= i; --i === i){
+//  if (i % 2 === 0) {
+//  $("#response").append("<br />" + (i) + " is even");
+//} else if ( i % 2 !== 0) {
+  //$("#response").append("<br />" + (i) + " is odd");
+//}
+//} ////runs backward from 4 to 0 - closest yet!
+
+//for (y, y === 0, ) {
+  //$("#response").append("<br>" + (i + 1) + " is even");
+  //i = i + 1;
+
+//switch (y) {
+//  case (y === 0):
+//  $("#response").append("<br>" + (x + 1) + " is even");
+//  break;
+//  case (y !== 0):
+// $("#response").append("<br>" + (x + 1) + " is odd");
+//  break;}
+
+//for (y === 0 ; x < i;){
+//    $("#response").append("<br>" + (x + 1) + " is even");
+//    x = x + 1;}
+//for (y !== 0 ; x < i;){
+//    $("#response").append("<br>" + (x + 1) + " is odd");
+//    x = x + 1;}
+
+//for (x === 0; i > x; x = i + 1) {
+//  $("#response").append("<br>" + i + " is even");
+//}
+
+// for (let y = i % 2; y !== 0; i < i + 1) {
+//    $("#response").append("<br>" + i + " is odd");
+
+
+//  i = i + 1;
+//} else {
+//  $("#response").append("<br>" + i + " is odd");
+//}
