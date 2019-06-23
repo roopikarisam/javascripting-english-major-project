0b707b42e70ff96ffa136ac12db321499b5e0cc1
Roopika Risam <rrisam@salemstate.edu>
2019-06-22 21:20:39 -0400

    added tipping calculator

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
-let i = prompt("givemeanumber");
-let x = 0;
+
+
+
+//TIPPING CALCULATOR Ch. 2
+let tipRate, bill, billPlusTip;
+tipRate = 0.20;
+bill = 10.00;
+billPlusTip = (bill * tipRate) + bill;
+console.log(billPlusTip);
+
+//COUNT UP TO USER INPUT
+//let i = prompt("givemeanumber");
+//let x = 0;
 //let y = i % 2;
 
 // this code also works and removes unnecessary line
-for (x  ; x <= i; x++) { // initializes var x, sets testing condition of when x is greater than or equal to i so the loop runs when x is less than i and stops when x = i, indicates that each time var x is run through the loop it increases by 1
-  if (x % 2 === 0) { // the loop iterates over x so we want to test x for its modulus - remainder (in this case when divided by 2) - if the modulus === 0 it's an even number) [my error before was using i]
-  $("#response").append("<br />" + x + " is even"); //prints even if condition (x % 2 === 0) is met
-  }
-  else {  // if first condition isn't met, prints odd
-  $("#response").append("<br />" + x + " is odd");
-}
-}
+//for (x  ; x <= i; x++) { // initializes var x, sets testing condition of when x is greater than or equal to i so the loop runs when x is less than i and stops when x = i, indicates that each time var x is run through the loop it increases by 1
+  //if (x % 2 === 0) { // the loop iterates over x so we want to test x for its modulus - remainder (in this case when divided by 2) - if the modulus === 0 it's an even number) [my error before was using i]
+//  $("#response").append("<br />" + x + " is even"); //prints even if condition (x % 2 === 0) is met
+//  }
+//  else {  // if first condition isn't met, prints odd
+//  $("#response").append("<br />" + x + " is odd");
+//}
+//}
 
 
 // THIS CODE ACTUALLY WORKS
