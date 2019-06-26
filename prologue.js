2c0fe387d9d3512aea10ac915e0bdd9881e8226e
Roopika Risam <rrisam@salemstate.edu>
2019-06-26 16:53:23 -0400

    added callback function

--- prologue.js
+++ prologue.js
@@ -1,5 +1,45 @@
+
+//clicky code using JSON data
 // 1. Set the content of #glosses
 $("#glosses").html("<p>The glosses will go here.</p>");
+// 2. Set the content of the Prologue
+$.getJSON("https://the-javascripting-english-major.org/v1/prologue.json", function (data){
+  let prologueText; //define the variable needed
+  prologueText = "<blockquote><p>"; // open the tags
+  //now iterate over the data variable's .lines property:
+  data.lines.forEach(function(line){ //returns a variable, lines
+  //define blank lineText.
+  let lineText;
+  lineText = "";
+// now iterate over each line. should be familiar
+  line.forEach(function(word){
+    let wordString;
+    wordString = word.text;
+    if (word.modern){
+      wordString = "<a href='#' data-modern='" + word.modern + "'>" +wordString + "</a>";
+    }
+    lineText = lineText + wordString + " ";
+  });
+// Add lineText with a line break to the prologueText.
+prologueText = prologueText + lineText + "<br/>";
+});
+//close the prologueText tags.
+prologueText = prologueText + "</p></blockquote>";
+//replace the content of #prologue.
+$("#prologue").html(prologueText);
+}); //close callback function & method
+//3. Wait for user to click on an <a> tag inside #prologue and then change the content of #glosses.
+$("#prologue a").click(function(){
+  let glossText, clickedWord, modernWord;
+  clickedWord = $(this).text(); //when called w/o parameters, .text() method gets text; when called w/ parameters it sets the text to the parameter.
+  modernWord = $(this).data("modern"); //gets data for modern
+  glossText = "<h2>You clicked " + clickedWord + ", which means " + modernWord + "</h2>";
+  $("#glosses").html(glossText);
+}); //this is going to be helpful to try and get stuff to show up in a sidebar on the data viz
+
+//working clicky code
+// 1. Set the content of #glosses
+/* $("#glosses").html("<p>The glosses will go here.</p>");
 // 2. Set the content of #prologue
 let line1, line1Text; // don’t need the intermediate step of line1TextArray
 line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill," , modern: "April,"}, {text: "with"},
@@ -17,11 +57,11 @@ line1.forEach(function(word){
 $("#prologue").html(line1Text);
 // <br /> makes a line break, which will come in handy when we have many
 // lines.
-//3. Wait for user to click on an <a> tag inside #prologue and then change the content of #glosses. 
+//3. Wait for user to click on an <a> tag inside #prologue and then change the content of #glosses.
 $("#prologue a").click(function(){
   let glossText, clickedWord, modernWord;
   clickedWord = $(this).text(); //when called w/o parameters, .text() method gets text; when called w/ parameters it sets the text to the parameter.
   modernWord = $(this).data("modern"); //gets data for modern
   glossText = "<h2>You clicked " + clickedWord + ", which means " + modernWord + "</h2>";
   $("#glosses").html(glossText);
-}); //this is going to be helpful to try and get stuff to show up in a sidebar on the data viz
+}); //this is going to be helpful to try and get stuff to show up in a sidebar on the data viz */
