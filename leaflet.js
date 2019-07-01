b395856c249cc5da768dfd5f9491170775219ed0
Roopika Risam <rrisam@salemstate.edu>
2019-07-01 08:38:46 -0400

    commented on creating tile layer

--- leaflet.js
+++ leaflet.js
@@ -5,6 +5,6 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
       maxZoom: 18
-    }); //defining tileLayer - this alone does not add tile layer, need to indicate what part of the world to show.
+    }); //defining tileLayer, specificies server from which Leaflet should get tiles (here, cartocdn.com) and add three options (attribution is what appears in the bottom corner of the map) - this alone does not add tile layer, need to indicate what part of the world to show.
 tileLayer.addTo(firstMap); //all leaflet objects have to be added to the map using the .addTo() method.
-firstMap.setView([40.730833, -73.9975], 16); //lines 9 & 10 set initial map view. Map itself has a method, .setView() that has two parameters - array of coordinates adn zoom level. Highest zoom level is 19. 
+firstMap.setView([40.730833, -73.9975], 16); //lines 9 & 10 set initial map view. Map itself has a method, .setView() that has two parameters - array of coordinates adn zoom level. Highest zoom level is 19.
