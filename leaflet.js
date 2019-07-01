bfdcc36fdf5a85eb2f480e2b9a50c14e4d914098
Roopika Risam <rrisam@salemstate.edu>
2019-07-01 08:30:58 -0400

    added leaflet map

---
+++ leaflet.js
@@ -0,0 +1,2 @@
+let firstMap;
+firstMap = L.map("first-map"); //first map is now object to use in JS to control map. .map() is a method that L (Leaflet) has that creates a map in the <div> with the id given as the parameter. [firstmap div is in the htmls file]
