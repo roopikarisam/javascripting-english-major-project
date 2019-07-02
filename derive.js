73c95332e2ef3f72441c2799aa4f7c580ccec6e5
Roopika Risam <rrisam@salemstate.edu>
2019-07-01 20:54:08 -0400

    drew lines on map

--- derive.js
+++ derive.js
@@ -9,6 +9,24 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(firstMap); //all leaflet objects have to be added to the map using the .addTo() method.
 firstMap.setView([42.52261, -70.88634], 16); //lines 9 & 10 set initial map view. Map itself has a method, .setView() that has two parameters - array of coordinates adn zoom level. Highest zoom level is 19.
 
-let derive1, derive1Marker;
+let derive1, derive1Marker, derive1Line1Polyline;
 derive1 = L.latLng(42.52261, -70.88634);
 derive1Marker = L.marker(derive1).addTo(firstMap); //note capitalization of .addTo() method and lowercase of m in marker
+
+let derive1Line1 = [[derive1.lat, derive1.lng], //sets beginning of line at marker/beginning point
+[derive1.lat - 0.00068, derive1.lng + 0.00045],
+[derive1.lat - 0.00185, derive1.lng - 0.00243],
+[derive1.lat + 0.00002, derive1.lng - 0.00343]]; //sets coords for polylines
+derive1Line1Polyline = L.polyline(derive1Line1, {color: "#db6e35"}
+).addTo(firstMap);
+
+let derive2, derive2Marker, derive2Line1Polyline;
+derive2 = L.latLng(42.52261, -70.88634);
+derive2Marker = L.marker(derive2).addTo(firstMap); //note capitalization of .addTo() method and lowercase of m in marker
+
+let derive2Line1 = [[derive2.lat, derive2.lng], //sets beginning of line at marker/beginning point
+[derive2.lat + 0.00068, derive2.lng + 0.00045],
+[derive2.lat + 0.00185, derive2.lng - 0.00243],
+[derive2.lat - 0.00002, derive2.lng - 0.00343]]; //sets coords for polylines
+derive2Line1Polyline = L.polyline(derive2Line1, {color: "#d33682"}
+).addTo(firstMap);
